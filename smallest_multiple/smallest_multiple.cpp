053e681fcff0782f0258356dda62676a3cebb472
Dexter575 <idreees575@gmail.com>
2020-02-17 19:43:25 +0500

    refactor: enhance code

--- smallest_multiple/smallest_multiple.cpp
+++ smallest_multiple/smallest_multiple.cpp
@@ -1,47 +1,35 @@
 //Problem 5
 //Smallest multiple
 
-
 // ---Problem Statement
 // ---2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 // ---What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 // ---Solution
 
-
-
 #include "bits/stdc++.h"           //is a Header File Includes every SL
 #include <iostream>
 #include <stdio.h>
 
-
-using namespace std;
-
-int print_vect(vector<int> vect);
-int calculate_smallest_positive_number(int low, int high);
+int PrintVect(std::vector<int> vect);
+int CalculateSmallestPositiveNumber(int low, int high);
 
 int main()
 {
     int lowest_range = 1;
     int highest_range = 10;
-
-    cout<<"Smallest Possible Integer is: "<<endl;
-    cout<< calculate_smallest_positive_number(lowest_range, highest_range);
-
-
+    std::cout<<"Smallest Possible Integer is: "<<std::endl;
+    std::cout<< CalculateSmallestPositiveNumber(lowest_range, highest_range);
 }
 
-int calculate_smallest_positive_number(int low, int high)
+int CalculateSmallestPositiveNumber(int low, int high)
 {
-    //Initalize a Vector
-    vector<int> vect(high);
+    //Initalize a vector
+    std::vector<int> vect(high);
     iota(begin(vect), end(vect), 1);
-
-    print_vect(vect);
-
+    PrintVect(vect);
     bool check_i = false;
     int integer = 0;
-
     for(int i = 1; check_i == false; i++)
     {
         for(int j =1; j <= high; j++)
@@ -66,15 +54,11 @@ int calculate_smallest_positive_number(int low, int high)
     return integer;
 }
 
-
-
-int print_vect(vector<int> vect)
+int PrintVect(std::vector<int> vect)
 {
     //Display the vectors (multiples)
-        for(int i = 0; i < vect.size(); i++)
-        {
-            cout<<vect[i]<<' ';
-        }
-        cout<<endl;
+    for(int i = 0; i < vect.size(); i++)
+        std::cout<<vect[i]<<' ';
+    std::cout<<std::endl;
     return 0;
 }
--- sum_square_diff/sum_square_diff.cpp
+++ sum_square_diff/sum_square_diff.cpp
@@ -1,7 +1,6 @@
 //Problem 6
 //Sum square difference
 
-
 // ---Problem Statement
 // ---The sum of the squares of the first ten natural numbers is, 385
 // ---The square of the sum of the first ten natural numbers is, 3025
@@ -10,45 +9,35 @@
 
 // ---Solution
 
-
-
-
-#include "bits/stdc++.h"           //is a Header File Includes every SL
 #include <iostream>
 #include <stdio.h>
 #include<cmath>
 
-using namespace std;
-int calc_sum_of_squares(int number);
-int calc_square_of_sums(int number);
+int CalcSumOfSquares(int number);
+int CalcSquareOfSums(int number);
 
 int main()
 {
     int n = 10;
-    cout<< "Sum of Squares: "<<calc_sum_of_squares(n);
-    cout<< "Square of Sums: "<<calc_square_of_sums(n);
-    
-    
+    std::cout<< "Sum of Squares: "<<CalcSumOfSquares(n);
+    std::cout<< "Square of Sums: "<<CalcSquareOfSums(n);
     return 0;
 }
 
-
-int calc_sum_of_squares(int number)
+int CalcSumOfSquares(int number)
 {
     int sum_of_squares = (number+1) * (number + 0.5) * (number/3);
     return sum_of_squares;
 }
 
-int calc_square_of_sums(int number)
+int CalcSquareOfSums(int number)
 {
     int square_of_sums = number * (number +1 );
     square_of_sums /= 2;
-    
     return square_of_sums;
 }
 
-
-int calc_square(int num)
+int CalcSquare(int num)
 {
     pow(num,2);
     return num;
